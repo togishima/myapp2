cecacab62bfaf066ce530baa02fb501681d5e8e8
togishima <t.ogi.1211@gmail.com>
2020-09-14 19:12:02 +0900

    upd

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,15 +6,17 @@
 
 @section('content')
 <div class="container">
-   <div class="row justify-content-center">
-       <div class="col-md-8">
-           <div class="card">
-              <div class="card-header">{{ $calendar->getTitle() }}</div>
-              <div class="card-body">
-					        {!! $calendar->render() !!}
-              </div>
-            </div>
-       </div>
-   </div>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">
+						{{ $calendar->getTitle() }}
+				</div>
+				<div class="card-body">
+						{!! $calendar->render() !!}
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
